a3f2964a8d271bfb622535b7d9e716bcd4e41a3c
fiqAsh <ashfiq625@gmail.com>
2024-10-08 21:44:36 +0600

    signup controller complete

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,72 @@
+import User from "../models/user.model.js";
+
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+
 export const signup = async (req, res) => {
-	res.json({
-		data: "signup end point",
-	});
+	try {
+		const { fullName, username, email, password } = req.body;
+
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(email)) {
+			return res.status(400).json({ error: "invalid email format" });
+		}
+
+		const existingUser = await User.findOne({ username });
+
+		if (existingUser) {
+			return res.status(400).json({ error: "username taken" });
+		}
+
+		const existingEmail = await User.findOne({ email });
+
+		if (existingEmail) {
+			return res.status(400).json({ error: "Email taken" });
+		}
+
+		if (password.length < 6) {
+			return res
+				.status(400)
+				.json({ error: "password must be more than 6" });
+		}
+
+		//hash password
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const newUser = new User({
+			fullName,
+			username,
+			email,
+			password: hashedPassword,
+		});
+
+		if (newUser) {
+			generateTokenAndSetCookie(newUser._id, res);
+			await newUser.save();
+			res.status(201).json({
+				_id: newUser._id,
+				fullname: newUser.fullName,
+				username: newUser.username,
+				email: newUser.email,
+				followers: newUser.followers,
+				following: newUser.following,
+				profileImg: newUser.profileImg,
+				coverImg: newUser.coverImg,
+			});
+		} else {
+			res.status(400).json({ error: "invalid user data" });
+		}
+	} catch (error) {
+		console.log("error in signup controller", error.message);
+		res.status(500).json({ error: "internal server error" });
+	}
 };
 export const login = async (req, res) => {
-	res.json({
-		data: "login end point",
-	});
+	try {
+	} catch (error) {
+		console.log("error in signup controller", error.message);
+		res.status(500).json({ error: "internal server error" });
+	}
 };
 export const logout = async (req, res) => {
 	res.json({
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //to parse from data (urlencoded)
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
