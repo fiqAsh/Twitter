d98b8cf33460095ba63534995ea7bfdac6a4c778
fiqAsh <ashfiq625@gmail.com>
2024-10-09 01:50:15 +0600

    followunfollowuser, notificationModel

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,65 @@
+import Notification from "../models/notification.model.js";
+import User from "../models/user.model.js";
+
+export const getUserProfile = async (req, res) => {
+	const { username } = req.params;
+	try {
+		const user = await User.findOne({ username }).select("-password");
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+		res.status(200).json(user);
+	} catch (error) {
+		console.log("error in getuserprofile", error.message);
+		res.staus(500).json({ error: error.message });
+	}
+};
+
+export const followUnfollowUser = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const userToModify = await User.findById(id);
+		const currentUser = await User.findById(req.user._id);
+
+		if (id === req.user._id.toString()) {
+			return res
+				.status(400)
+				.json({ message: "you cant follow unfollow yourself" });
+		}
+		if (!userToModify || !currentUser) {
+			return res.status(400).json({ message: "user not found" });
+		}
+
+		const isFollowing = currentUser.following.includes(id);
+
+		if (isFollowing) {
+			await User.findByIdAndUpdate(id, {
+				$pull: { followers: req.user._id },
+			});
+			await User.findByIdAndUpdate(req.user._id, {
+				$pull: { following: id },
+			});
+			res.status(200).json({ message: "User unfollowed successfully" });
+		} else {
+			await User.findByIdAndUpdate(id, {
+				$push: { followers: req.user._id },
+			});
+			await User.findByIdAndUpdate(req.user._id, {
+				$push: { following: id },
+			});
+			//send notification to user
+
+			const newNotification = new Notification({
+				type: "follow",
+				from: req.user._id,
+				to: userToModify._id,
+			});
+			await newNotification.save();
+
+			res.status(200).json({ message: "User followed successfully" });
+		}
+	} catch (error) {
+		console.log("error in followunfollowuser", error.message);
+		res.staus(500).json({ error: error.message });
+	}
+};
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-
+import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 
 const app = express();
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true })); //to parse from data (urlencode
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
 	console.log(`server is running on ${PORT}`);
