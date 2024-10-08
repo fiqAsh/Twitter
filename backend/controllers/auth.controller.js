3a6d0514e5f66d9997437b48c745e3f6c85d9eef
fiqAsh <ashfiq625@gmail.com>
2024-10-08 22:19:42 +0600

    login logout protectedroute middleware getme

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -63,13 +63,48 @@ export const signup = async (req, res) => {
 };
 export const login = async (req, res) => {
 	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(
+			password,
+			user?.password || ""
+		);
+		if (!user || !isPasswordCorrect) {
+			return res
+				.status(400)
+				.json({ errod: "invalid username or password" });
+		}
+		generateTokenAndSetCookie(user._id, res);
+		res.status(200).json({
+			_id: user._id,
+			fullname: user.fullName,
+			username: user.username,
+			email: user.email,
+			followers: user.followers,
+			following: user.following,
+			profileImg: user.profileImg,
+			coverImg: user.coverImg,
+		});
 	} catch (error) {
 		console.log("error in signup controller", error.message);
 		res.status(500).json({ error: "internal server error" });
 	}
 };
 export const logout = async (req, res) => {
-	res.json({
-		data: "logout end point",
-	});
+	try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "logout successful" });
+	} catch (error) {
+		res.status(500).json({ error: "internal server error" });
+	}
+};
+
+export const getMe = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id).select("-password");
+		res.status(200).json(user);
+	} catch (error) {
+		console.log("error in getme controller");
+		res.status(500).json({ error: "internal server error" });
+	}
 };
