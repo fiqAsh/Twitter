2784a596ea09ce5c9f225118f43766defd0528c0
fiqAsh <ashfiq625@gmail.com>
2024-10-08 19:46:24 +0600

    setup mongo db and basic routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+	res.json({
+		data: "signup end point",
+	});
+};
+export const login = async (req, res) => {
+	res.json({
+		data: "login end point",
+	});
+};
+export const logout = async (req, res) => {
+	res.json({
+		data: "logout end point",
+	});
+};
