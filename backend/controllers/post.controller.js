df51e14e7d289867861efe9b7fa409af573d3379
fiqAsh <ashfiq625@gmail.com>
2024-10-10 19:23:25 +0600

    delete post comment ON post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -33,3 +33,58 @@ export const createPost = async (req, res) => {
 		res.status(500).json({ error: "internal server error" });
 	}
 };
+
+export const deletePost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "post not found" });
+		}
+
+		if (post.user.toString() !== req.user._id.toString()) {
+			return res
+				.status(401)
+				.json({ error: "not authorized to delete this post" });
+		}
+
+		if (post.img) {
+			const imgId = post.img.split("/").pop().split(".")[0];
+			await cloudinary.uploader.destroy(imgId);
+		}
+
+		await Post.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "post deleted successfully" });
+	} catch (error) {
+		console.log("error in deletepost cotroller");
+		res.status(500).json({ error: "internal server error" });
+	}
+};
+
+export const commentOnPost = async (req, res) => {
+	try {
+		const { text } = req.body;
+		const postId = req.params.id;
+		const userId = req.user._id;
+
+		if (!text) {
+			return res.status(400).json({ error: "text field is required" });
+		}
+		const post = await Post.findById(postId);
+
+		if (!post) {
+			return res.status(404).json({ error: "post not found" });
+		}
+
+		const comment = { user: userId, text };
+
+		post.comments.push(comment);
+
+		await post.save();
+
+		res.status(200).json(post);
+	} catch (error) {
+		console.log("error in commentonPost cotroller", error);
+		res.status(500).json({ error: "internal server error" });
+	}
+};
