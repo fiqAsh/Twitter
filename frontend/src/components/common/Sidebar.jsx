9da6be5a0b85086a22cce37675ed8fe15f3b8ac2
fiqAsh <ashfiq625@gmail.com>
2024-10-13 18:02:37 +0600

    logout functionality

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,8 +5,33 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useMutation } from "react-query";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
+	const { mutate: logout } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch("/api/auth/logout", {
+					method: "POST",
+				});
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("logout successful");
+		},
+		onError: () => {
+			toast.error("logout successful");
+		},
+	});
+
 	const data = {
 		fullName: "John Doe",
 		username: "johndoe",
@@ -79,7 +104,13 @@ const Sidebar = () => {
 									@{data?.username}
 								</p>
 							</div>
-							<BiLogOut className="w-5 h-5 cursor-pointer" />
+							<BiLogOut
+								className="w-5 h-5 cursor-pointer"
+								onClick={(e) => {
+									e.preventDefault();
+									logout();
+								}}
+							/>
 						</div>
 					</Link>
 				)}
